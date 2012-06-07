5e7880dda85ce79052408da75629ba7c91b4137d
Christian Perrier <bubulle@debian.org>
2012-06-07 06:52:11 +0000

    Load geneweb-6.05.1 into deb-maint/geneweb/branches/upstream.

--- src/compilation.ml
+++ src/compilation.ml
@@ -1,4 +1,4 @@
 open Def;
-value (wd, d, h) = (1, {day=04;month=06;year=2012;prec=Sure;delta=0}, "11:04:35 CEST");
+value (wd, d, h) = (2, {day=05;month=06;year=2012;prec=Sure;delta=0}, "08:59:42 CEST");
 value ct conf = Printf.sprintf " (%4d-%02d-%02d %s)" d.year d.month d.day h;
 Util.compilation_time_hook.val := ct;
